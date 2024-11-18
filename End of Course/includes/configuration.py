65e9e33abaf6c1a2008bb5d550757ae453b059f3
Swathi-Dataanalysis <160053816+Swathi-Dataanalysis@users.noreply.github.com>
2024-11-18 12:48:46 -0700

    Add files via upload

---
+++ End of Course/includes/configuration.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+raw_folder_path = "/mnt/formula1dlpro2/raw"
+processed_folder_path = "/mnt/formula1dlpro2/processed"
+presentation_folder_path = "/mnt/formula1dlpro2/presentation"
+
+# COMMAND ----------
+
