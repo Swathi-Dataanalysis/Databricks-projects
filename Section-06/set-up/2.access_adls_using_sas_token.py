512fc27df741d3b4c72d594e384522aee0841735
Swathi-Dataanalysis <160053816+Swathi-Dataanalysis@users.noreply.github.com>
2024-11-18 12:55:13 -0700

    Update 2.access_adls_using_sas_token.py

--- Section-06/set-up/2.access_adls_using_sas_token.py
+++ Section-06/set-up/2.access_adls_using_sas_token.py
@@ -8,14 +8,13 @@
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC # SAS token
-# MAGIC sp=rl&st=2024-11-17T17:59:06Z&se=2024-11-19T01:59:06Z&spr=https&sv=2022-11-02&sr=c&sig=uKE7d0dLedB21%2FQlFUGRD5oJ0WttrkcAAaeGPFSPszY%3D
+
 
 # COMMAND ----------
 
 spark.conf.set("fs.azure.account.auth.type.formula1dlpro2.dfs.core.windows.net", "SAS")
 spark.conf.set("fs.azure.sas.token.provider.type.formula1dlpro2.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.sas.FixedSASTokenProvider")
-spark.conf.set("fs.azure.sas.fixed.token.formula1dlpro2.dfs.core.windows.net", "sp=rl&st=2024-11-17T17:59:06Z&se=2024-11-19T01:59:06Z&spr=https&sv=2022-11-02&sr=c&sig=uKE7d0dLedB21%2FQlFUGRD5oJ0WttrkcAAaeGPFSPszY%3D")
+spark.conf.set("fs.azure.sas.fixed.token.formula1dlpro2.dfs.core.windows.net", "YOUR SAS TOKEN")
 
 # COMMAND ----------
 
